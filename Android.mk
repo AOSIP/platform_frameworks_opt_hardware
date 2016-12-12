be23081fbf9da03ce7419f571edc897fa1821cbf
B--B <dark.schneider.ds@gmail.com>
2016-12-13 00:32:42 +0100

    CMHW: remove all not needed stuffs and fix compiling for N
    I need only KeyDisabler, everything else can be removed

--- Android.mk
+++ Android.mk
@@ -43,7 +43,6 @@ LOCAL_SRC_FILES += $(default_classes) $(unique_specific_classes)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := org.cyanogenmod.hardware
-LOCAL_JAVA_LIBRARIES := org.cyanogenmod.platform.internal
 
 include $(BUILD_JAVA_LIBRARY)
 
